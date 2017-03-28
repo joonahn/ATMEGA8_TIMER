d15fc527cc766a53ad42d3f4de44e8e1085b6e4d
joonahn <sdeveloper@naver.com>
2017-03-28 19:58:14 +0900

    main code added

--- ATMEGA8_TIMER/main.cpp
+++ ATMEGA8_TIMER/main.cpp
@@ -1,18 +1,51 @@
-/*
- * ATMEGA8_TIMER.cpp
- *
- * Created: 2017-03-28 오후 7:53:30
- * Author : REMOTE
- */ 
+#define F_CPU 8000000UL
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+volatile int counter = 0;
+volatile int b_timer = 0;
+volatile int r_timer = 0;
+
+ISR(TIMER0_OVF_vect)
+{
+	counter++;
+
+	if(counter % 61==0) //0.125초 경과
+	{
+		if(counter >= 488) //1초 경과
+		{	counter = 0;	
+			r_timer ++;		
+		}
+		
+		b_timer ++;
+		
+	}
+}
 
 
 int main(void)
 {
-    /* Replace with your application code */
     while (1) 
     {
+		DDRB = 0x01; // PB0 핀을 output mode로 설정
+		TIMSK = 0x01; // TIMER0 overflow interrupt 사용 설정
+		TCCR0 = 0x03; // prescalar를 64로 설정
+
+		sei(); // 사용 설정한 interrupt를 활성화
+
+		if(r_timer % 2 == 0){ //짝수초마다 실행
+			
+			if(b_timer %2 == 0) 
+			{
+				PORTB = 0x01;  
+			}
+			else 
+				PORTB = 0x00;  
+		}
+		else 
+			PORTB = 0x00;
+					
     }
 }
 
