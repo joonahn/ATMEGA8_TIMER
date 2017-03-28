a05a809e317d061d91770a2a1df703fd90237d3d
joonahn <sdeveloper@naver.com>
2017-03-28 19:57:48 +0900

    initial commit

---
+++ ATMEGA8_TIMER/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * ATMEGA8_TIMER.cpp
+ *
+ * Created: 2017-03-28 오후 7:53:30
+ * Author : REMOTE
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
